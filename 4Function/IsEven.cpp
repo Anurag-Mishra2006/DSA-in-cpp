aee874c5a7d282ebb1a29358045967bda0ee2690
Anurag-Mishra2006 <eranurag2006@gmail.com>
2024-09-24 12:34:56 +0530

    Initial commit

---
+++ 4Function/IsEven.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+bool isEven(int a){
+    if (a&1)return 0;// odd
+    else return 1; //even
+
+
+}
+int main(){
+    int num;
+    cin>>num;
+    
+    if( isEven(num)==1){
+        cout<<"Number is Even "<<endl;
+    }
+    else cout<<"Number is Odd ";
+}
