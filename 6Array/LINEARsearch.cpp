c0ffe39f7cad89c12958945349992c2b7e1ccc18
Anurag-Mishra2006 <eranurag2006@gmail.com>
2024-10-06 16:46:14 +0530

    first commit

--- 6Array/LINEARsearch.cpp
+++ 6Array/LINEARsearch.cpp
@@ -15,9 +15,9 @@ int main(){
     bool flag=false;//false means not present
     for(int i=0;i<=n-1;i++ ){
 
-        if(arr[i]==x) flag=true;
+        if(arr[i]==x)flag=true;
     }
-    if(flag=true)
+    if(flag==true)
 cout<<"Element found";
 else cout<<"404 ERROR elemnt not found";
 }
