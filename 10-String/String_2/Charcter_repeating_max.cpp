58da565d9ae00e056ed70e8282f21e12203a87b8
Anurag-Mishra2006 <eranurag2006@gmail.com>
2024-10-09 15:44:57 +0530

    first commit

--- 10-String/String_2/Charcter_repeating_max.cpp
+++ 10-String/String_2/Charcter_repeating_max.cpp
@@ -1,8 +1,8 @@
+/* Given a string consisting of lowercase English alphabets. Print the char that is occuring most number of times  */
 #include<iostream>
 #include<string>
 #include<algorithm>
 using namespace std;
 int main(){                                                                                                                                 
-    // Code
-    
+    // Code 
 }
