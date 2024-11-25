35539de0b74bb1d3e2de0ce16ee76d5bfe31870c
Anurag-Mishra2006 <eranurag2006@gmail.com>
2024-11-25 19:45:50 +0530

    first commit

--- 14-Recurrtion/Recurtion1/Stair.cpp
+++ 14-Recurrtion/Recurtion1/Stair.cpp
@@ -8,7 +8,8 @@ int stair (int n){
     // stair(2)=2; stair(1)=1; base case
     if(n==2) return 2;
     if (n==1) return 1;
-    return stair(n-1) + stair(n-2) ;
+    return stair(n-1) + stair(n-2) ; //look like fibonacci
+    
 
 }
 // When three jumps are allowed 
