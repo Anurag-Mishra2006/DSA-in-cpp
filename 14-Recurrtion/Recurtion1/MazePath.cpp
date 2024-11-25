bb5e0d345909735bdbc432721231563ecce12cd0
Anurag-Mishra2006 <eranurag2006@gmail.com>
2024-11-25 22:18:00 +0530

    first commit

--- 14-Recurrtion/Recurtion1/MazePath.cpp
+++ 14-Recurrtion/Recurtion1/MazePath.cpp
@@ -2,6 +2,39 @@
 // You have to return no. of ways
 #include<iostream> 
 using namespace std;
-int main(){
+int maze(int startRow, int startColumn , int endRow , int endColumn){
+    if(startRow> endRow || startColumn>endColumn)return 0;
+    if(startRow==endRow && startColumn==endColumn)return 1;
+    int rightways =maze(startRow,startColumn+1,  endRow , endColumn);
+    int downways = maze(startRow+1,startColumn,  endRow , endColumn);
+    int totalways = rightways+ downways;
+    return totalways;
     
+}
+void printPath(int startRow, int startColumn , int endRow , int endColumn,string s){
+    if(startRow> endRow || startColumn>endColumn)return ;
+    if(startRow==endRow && startColumn==endColumn){
+        cout<<s<<endl;
+        return;
+    }  
+    printPath(startRow,startColumn+1,  endRow , endColumn,s+'R');//right
+    printPath(startRow+1,startColumn,  endRow , endColumn,s+'D');//down
+    
+}
+int maze2(int endrow,int endcolumn){
+    if(endrow<1 || endcolumn<1){return 0;}
+    if(endrow==1 && endcolumn==1){
+        return 1;
+    }
+    int rightways = maze2(endrow ,endcolumn-1);//right
+    int downways = maze2(endrow-1, endcolumn);//down
+     int totalways = rightways+ downways;
+    return totalways;
+}
+int main(){
+    //  if the person reaches destination then return 1
+    // if person escape out of matrix then return 0
+    cout<<maze(1,1,4,3)<<endl;
+    printPath(1,1,4,3,"");
+    cout<<maze2(4,3)<<endl;
 }
