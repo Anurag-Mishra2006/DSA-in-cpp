3540621a585d2052ca7183cee0ea99affce99e2d
Anurag-Mishra2006 <eranurag2006@gmail.com>
2024-11-25 18:48:48 +0530

    first commit

--- 14-Recurrtion/Recurtion1/fibonacci.cpp
+++ 14-Recurrtion/Recurtion1/fibonacci.cpp
@@ -3,11 +3,13 @@ using namespace std;
 int fibo(int n)
 {
     // recursion is based on the recurrence relation / formula
-    
+
     if (n == 1 || n == 2)
         return 1;
     return fibo(n - 1) + fibo(n - 2); // here fibo call itself twice'
-    // Recursive Tree ===> Euler Tour Tree Diagram
+    // firstly fibo(n-1) run completely then fibo(n-2) will runs
+    //  And this is true in every steps whenever there is sum of two recursive fuctions
+    // Recursive Tree ====> Euler Tour Tree Diagram
 }
 int main()
 {
