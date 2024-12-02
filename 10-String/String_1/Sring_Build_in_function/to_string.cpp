fb0ad6e81abc4a84b59b4d36832510a2fa512304
Anurag-Mishra2006 <eranurag2006@gmail.com>
2024-12-02 13:35:20 +0530

    first commit

--- 10-String/String_1/Sring_Build_in_function/to_string.cpp
+++ 10-String/String_1/Sring_Build_in_function/to_string.cpp
@@ -4,6 +4,8 @@
 using namespace std;
 int main(){
     int x=1234567;
+    //  to_string is used to convert both interger to long long datatype to string 
+    
     string s=to_string(x);
     cout<<s;
 }
