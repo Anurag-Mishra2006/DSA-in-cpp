7ae13e23c37da594a4f503c2d383b3414114a8df
Anurag-Mishra2006 <eranurag2006@gmail.com>
2024-12-02 13:11:34 +0530

    first commit

--- 10-String/String_2/Stringstream_class.cpp
+++ 10-String/String_2/Stringstream_class.cpp
@@ -6,12 +6,17 @@ using namespace std;
 int main(){
     string str;
     getline(cin,str);
+    //  We use stringstream when we do not want extra space between the words
+    /*
+            hello everyone    today my physics mark bataya            gaya 
+            helloeveryonetodaymyphysicsmarkbatayagaya
+    */
     stringstream ss(str);//stringstream ko naam bhi dete h (here it is ss).
     string temp;
 
     while(ss>>temp){//ss me se le   input lo
     
-        cout<<temp<<endl;
+        cout<<temp<<endl ;
     }
 
     
