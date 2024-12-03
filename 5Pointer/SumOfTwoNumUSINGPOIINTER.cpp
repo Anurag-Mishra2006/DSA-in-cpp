2d6cd639b89f5ffd16e6d07349afa74a635cd595
Anurag-Mishra2006 <eranurag2006@gmail.com>
2024-12-03 15:50:17 +0530

    first commit

--- 5Pointer/SumOfTwoNumUSINGPOIINTER.cpp
+++ 5Pointer/SumOfTwoNumUSINGPOIINTER.cpp
@@ -6,5 +6,9 @@ int main(){
     int *p1=&x;
     int *p2=&y;
     cout<< x + *p2;
+    cout<<endl;
+    *p1 = 93; // with the help of pointer we change the value of the variable
+    
+    cout<<x + *p2;
 
 }
